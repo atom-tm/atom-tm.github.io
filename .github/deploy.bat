c99b7d4f0170ca03face9fc67798eab09ca1122d
QuickSilver <avataraang399@gmail.com>
2019-10-27 13:53:31 +1000

    Update deployment
    Former-commit-id: a49687880d2dbb01b74bf0ad5bc967dfe60a6c10

--- .github/deploy.bat
+++ .github/deploy.bat
@@ -1,8 +1,9 @@
 @echo off
+pushd %~dp0\..
 yarn build &&  ^
 cd dist && ^
 git init && ^
 git add -A && ^
-git commit -m 'Deploy' && ^
+git commit -m 'Deploy to GitHub pages' && ^
 git push -f git@github.com:atom-tm/atom-tm.github.io.git master && ^
-cd ..
+popd
--- .github/deploy.sh
+++ .github/deploy.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env sh
 set -e
+pushd $(dirname $0)/..
 yarn build
 cd dist
 git init
 git add -A
-git commit -m 'Deploy'
+git commit -m 'Deploy to GitHub pages'
 git push -f git@github.com:atom-tm/atom-tm.github.io.git master
-cd -
+popd
