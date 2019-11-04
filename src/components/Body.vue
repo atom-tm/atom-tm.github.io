348e9665f566844238ed1fd7d8174145d6c0f263
Favoras <favoras142@gmail.com>
2019-11-04 17:41:06 +0200

    Test commit for Kasai
    Former-commit-id: b7b138e30f1b01237de730ee7646c1c6ec692f95

--- src/components/Body.vue
+++ src/components/Body.vue
@@ -106,10 +106,5 @@
 </script>
 
 <style lang="scss">
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-    }
 
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -29,10 +29,5 @@
 </script>>
 
 <style lang="scss">
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-    }
 
 </style>
