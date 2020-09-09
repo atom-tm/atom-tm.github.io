25d0613ca63fc4b1ac0a22d60dfeafc4bc076e6f
abelidze <abelidze@users.noreply.github.com>
2020-09-09 20:03:51 +0000

    Deploy to GitHub pages

---
+++ js/spacer.js
@@ -0,0 +1,66 @@
+(function () {
+
+    let navBarId = 'atom-navbar';
+    let spacerBlockId = 'atom-spacer';
+    let ancorClass = 'atom-ancor';
+
+
+    /** @description Установка верхнего отступа сайту
+     * @param {string} navBarId Id блока навигации
+     * @param {string} spacerBlockId Id блока устанавливающего отступ
+     */
+    function setPaddingTop(navBarId, spacerBlockId) {
+        let navBarHeight = getNavHeight(navBarId);
+        let spacerBlock = document.getElementById(spacerBlockId);
+        if (spacerBlock) {
+            spacerBlock.style.height = navBarHeight + "px";
+        }
+    }
+
+
+    /** @description Получить высоту блока навигации
+     * @param {string} navBarId Id блока навигации
+     * @param {number} Высота блока навигации
+     */
+    function getNavHeight(navBarId) {
+        let navBar = document.getElementById(navBarId);
+        if (navBar) {
+            return navBar.offsetHeight;
+        } else {
+            return 0;
+        }
+    }
+
+
+    /** @description Установка смещения якорям в зависимости от высоты блока навигации
+     * @param {string} navBarId Id блока навигации
+     * @param {string} ancorClass Имя класса элементов, которым требуется задавать смещение
+     */
+    function setAncorOffset(navBarId, ancorClass) {
+        let offset = getNavHeight(navBarId)
+        let ancorsList = document.getElementsByClassName(ancorClass);
+        for (ancor of ancorsList) {
+            ancor.style.marginTop = -offset + "px";
+        }
+    }
+
+
+    /** @description Перемещение к указанному в адресной строке якорю */
+    function goToAncorFromHash() {
+        if (window.location.hash) {
+            window.location = window.location;
+        }
+    }
+
+    window.addEventListener("load", () => {
+        setPaddingTop(navBarId, spacerBlockId);
+        setAncorOffset(navBarId, ancorClass);
+        goToAncorFromHash();
+    });
+
+    window.addEventListener("resize", () => {
+        setPaddingTop(navBarId, spacerBlockId);
+        setAncorOffset(navBarId, ancorClass);
+    });
+
+})();
